d46150424307ad9c1b4f5aef81fb8694d0c02b12
konorevph <regouserk@mail.ru>
2023-12-12 10:04:46 +0300

    Comments has been added to Map

--- src/Map/ADTList/Map.java
+++ src/Map/ADTList/Map.java
@@ -6,12 +6,19 @@ import Map.MapInterface;
 
 public class Map implements MapInterface{
     private List list = new List();
+
+    /*
+    Не зависимо от существования в отображении d, устанавливаем M(d) = r
+     */
     @Override
     public void assign(char[] d, char[] r) {
+        // Создаём объект для вставки в список. Берём позицию начала списка.
         PostalDelivery postalDelivery = new PostalDelivery(d, r);
         Position position = list.First();
 
+        // Цикл по списку
         while (!position.equals(list.End())) {
+            // Если нашли d в отображении, то заменяем значение для M(d) на r
             if (arrayEquals(list.Retrieve(position).name, d)) {
                 list.Insert(position, postalDelivery);
                 list.Delete(list.Next(position));
@@ -20,13 +27,19 @@ public class Map implements MapInterface{
             position = list.Next(position);
         }
 
+        // Если d не определено в отображении, то создаём новый
         list.Insert(position, postalDelivery);
     }
 
+    /*
+    Истина, если M(d) определено. В r записывается значение M(d).
+     */
     @Override
     public boolean compute(char[] d, char[] r) {
+        // Цикл по списку
         Position position = list.First();
         while (!position.equals(list.End())) {
+            // Вытягиваем значение из списка. Если d определено, то вставляем в r его значение и выводим true
             PostalDelivery postalDelivery = list.Retrieve(position);
             if (arrayEquals(postalDelivery.name, d)) {
                 System.arraycopy(postalDelivery.address, 0, r, 0, r.length);
@@ -37,6 +50,9 @@ public class Map implements MapInterface{
         return false;
     }
 
+    /*
+    Очищаем отображение.
+     */
     @Override
     public void makeNull() {
         list.MakeNull();
@@ -61,6 +77,9 @@ public class Map implements MapInterface{
         return result;
     }
 
+    /*
+    Проверка эквивалентности значений массивов
+     */
     private boolean arrayEquals(char[] A, char[] B) {
         if (A.length != B.length) return false;
         for (int i = 0; i < A.length; i++) {
--- src/Map/List/Map.java
+++ src/Map/List/Map.java
@@ -20,16 +20,21 @@ public class Map implements MapInterface{
 
     private Item head;
 
+    /*
+    Не зависимо от существования в отображении d, устанавливаем M(d) = r
+     */
     @Override
     public void assign(char[] d, char[] r) {
-
+        // Если список пуст
         if (head == null) {
             head = new Item(d, r);
             return;
         }
 
+        // Цикл по списку
         Item tmp = head, prev = null;
         while (tmp != null) {
+            // Если d определено, то заменяем для него значение
             if (arrayEquals(tmp.name, d)) {
                 tmp.address = r;
                 return;
@@ -38,14 +43,19 @@ public class Map implements MapInterface{
             tmp = tmp.next;
         }
 
+        // Если d не определено
         prev.next = new Item(d, r);
     }
 
+    /*
+    Истина, если M(d) определено. В r записывается значение M(d).
+     */
     @Override
     public boolean compute(char[] d, char[] r) {
-
+        // Цикл по списку
         Item tmp = head;
         while (tmp != null) {
+            //Если d определено, вставляем в r его значение и выводим true
             if (arrayEquals(tmp.name, d)) {
                 System.arraycopy(tmp.address, 0, r, 0, tmp.address.length);
                 return true;
@@ -55,6 +65,9 @@ public class Map implements MapInterface{
         return false;
     }
 
+    /*
+    Очищаем отображение.
+     */
     @Override
     public void makeNull() {
         head = null;
@@ -78,7 +91,9 @@ public class Map implements MapInterface{
         return result;
     }
 
-    // Если используем PostalDelivery
+    /*
+   Проверка эквивалентности значений массивов
+    */
     private boolean arrayEquals(char[] A, char[] B) {
         if (A.length != B.length) return false;
         for (int i = 0; i < A.length; i++) {
